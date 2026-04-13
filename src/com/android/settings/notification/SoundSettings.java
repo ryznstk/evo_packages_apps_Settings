baikalos/android_packages_apps_Settings

Settings: Set proper initial value for multi media focus
* Due to past commits, it is likely that this value is already set.
* Let's check when creating preferences.
* Also move settings under Audio sub-settings.

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/settings/notification/SoundSettings.java
@@ -26,6 +26,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
@@ -50,6 +51,8 @@ import com.android.settingslib.core.instrumentation.Instrumentable;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.preference.UtilsKt;
 import com.android.settingslib.search.SearchIndexable;
+
+import com.crdroid.settings.preferences.SystemSettingSwitchPreference;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -130,6 +133,12 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
             return null;
         });
         updateAmbientMusicPref();
+
+        boolean mediaFocus = Settings.System.getIntForUser(getContext().getContentResolver(),
+            Settings.System.MULTI_AUDIO_FOCUS_ENABLED, 0, UserHandle.USER_CURRENT) != 0;
+        SystemSettingSwitchPreference mediaFocusPref =
+            (SystemSettingSwitchPreference) findPreference("multi_audio_focus_enabled");
+        mediaFocusPref.setChecked(mediaFocus);
     }
 
     private void updateAmbientMusicPref() {

ryznstk/evo_packages_apps_Settings

src/com/android/settings/notification/SoundSettings.java
@@ -26,6 +26,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
@@ -49,6 +50,8 @@ import com.android.settingslib.core.instrumentation.Instrumentable;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.preference.UtilsKt;
 import com.android.settingslib.search.SearchIndexable;
+
+import org.evolution.settings.preferences.SystemSettingSwitchPreference;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -127,6 +130,12 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
             }
             return null;
         });
+
+        boolean mediaFocus = Settings.System.getIntForUser(getContext().getContentResolver(),
+            Settings.System.MULTI_AUDIO_FOCUS_ENABLED, 0, UserHandle.USER_CURRENT) != 0;
+        SystemSettingSwitchPreference mediaFocusPref =
+            (SystemSettingSwitchPreference) findPreference("multi_audio_focus_enabled");
+        mediaFocusPref.setChecked(mediaFocus);
     }
 
     @Override